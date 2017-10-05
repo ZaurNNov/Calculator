e3899cb6a7786d4d983ab8a11f9ca6d13f67c24c
User <user@n104523.merann.ru>
2017-10-05 18:44:39 +0300

    commit after end of second video in course

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,16 +13,21 @@ class ViewController: UIViewController
     
     @IBOutlet weak var display: UILabel!
     
+    private var brain = CalculatorBrain()
+    
     @IBAction func performOperation(_ sender: UIButton) {
-        userIsInTheMiddleOfTyping = false
+        
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        
         if let mathematicalSymbol = sender.currentTitle {
-            switch mathematicalSymbol {
-                
-            case "π" : displayValue = Double.pi
-            case "√" : displayValue = sqrt(displayValue)
-
-            default : break
-            }
+            brain.performOperation(mathematicalSymbol)
+        }
+        
+        if let result = brain.result {
+            displayValue = result
         }
     }
     
