17742b4252e23be6b61067ef25fa4e81a66205c5
User <user@n104523.merann.ru>
2017-10-11 10:05:47 +0300

    [Assignmen 1, fix description] fix multiple description in descriptionLabel

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -51,17 +51,17 @@ struct CalculatorBrain {
     private var operations: Dictionary<String,Operation> = [
         "π" : Operation.constant(Double.pi),
         "e" : Operation.constant(M_E),
-        "√" : Operation.unaryOperation(sqrt, {"√(" + $0 + ")"}),
-        "%" : Operation.binaryOperations({($0 / $1) * 100}, {$0 + " % " + $1}),
-        "cos" : Operation.unaryOperation(cos, {"cos(" + $0 + ")"}), // {"cos(" + $0 + ")"}
-        "sin" : Operation.unaryOperation(sin, {"sin(" + $0 + ")"}),
-        "tan" : Operation.unaryOperation(tan, {"tan(" + $0 + ")"}),
-        "ln" : Operation.unaryOperation(log, {"ln(" + $0 + ")"}),
-        "±" : Operation.unaryOperation({ -$0 }, {"±(" + $0 + ")"}),  // {"±(" + $0 + ")"})
-        "×" : Operation.binaryOperations(*, {"(" + $0 + ")x" + $1}), // {"(" + $0 + ")x" + $1}
-        "÷" : Operation.binaryOperations(/, {$0 + " ÷ " + $1}),
-        "+" : Operation.binaryOperations(+, {$0 + " + " + $1}),
-        "−" : Operation.binaryOperations(-, {$0 + " - " + $1}), // {$0 + " - " + $1}
+        "√" : Operation.unaryOperation(sqrt, nil), //{"√(" + $0 + ")"}
+        "%" : Operation.binaryOperations({($0 / $1) * 100}, nil), //{$0 + " % " + $1}
+        "cos" : Operation.unaryOperation(cos, nil), // {"cos(" + $0 + ")"}
+        "sin" : Operation.unaryOperation(sin, nil), //{"sin(" + $0 + ")"}
+        "tan" : Operation.unaryOperation(tan, nil), //{"tan(" + $0 + ")"}
+        "ln" : Operation.unaryOperation(log, nil), //{"ln(" + $0 + ")"}
+        "±" : Operation.unaryOperation({ -$0 }, nil),  // {"±(" + $0 + ")"}
+        "×" : Operation.binaryOperations(*, {"(" + $0 + ") x " + $1}), // {"(" + $0 + ")x" + $1}
+        "÷" : Operation.binaryOperations(/, nil), //{$0 + " ÷ " + $1}
+        "+" : Operation.binaryOperations(+, nil), //{$0 + " + " + $1}
+        "−" : Operation.binaryOperations(-, nil), // {$0 + " - " + $1}
         "=" : Operation.equals,
         "Rand" : Operation.nullOperation({Double(arc4random())/Double(UInt32.max)}, "Rand()")
     ]
@@ -88,17 +88,15 @@ struct CalculatorBrain {
             switch operation {
             
             case .nullOperation(let function, let descriptionFunction):
-                cashe.accumulator = function()
-                cashe.descriptionAccumulator = descriptionFunction
+                cashe = (function(), descriptionFunction)
                 
             case .constant(let value):
-                cashe.accumulator = value
-                cashe.descriptionAccumulator = symbol
+                cashe = (value, symbol)
                 
             case .unaryOperation(let function, var descriptionFunction):
                 if cashe.accumulator != nil {
                     cashe.accumulator = function(cashe.accumulator!)
-                    if description == nil {
+                    if descriptionFunction == nil {
                         descriptionFunction = {symbol + "(" + $0 + ")"}
                     }
                     cashe.descriptionAccumulator = descriptionFunction!(cashe.descriptionAccumulator!)
@@ -107,7 +105,7 @@ struct CalculatorBrain {
             case .binaryOperations(let function, var descriptionFunction):
                 performPendingBinaryOperation()
                 if cashe.accumulator != nil  {
-                    if description == nil {
+                    if descriptionFunction == nil {
                         descriptionFunction = {$0 + " " + symbol + " " + $1}
                     }
                 
