8765d18fbde1c11247719c0ef98707cce4e8b49b
Zaur Giyasov <zaurusnnov@icloud.com>
2017-10-13 06:11:59 +0300

    add axes

--- Calculator/GraphicView.swift
+++ Calculator/GraphicView.swift
@@ -13,10 +13,32 @@ class GraphicView: UIView {
     //model test y = f(x)
     var yForX: ((Double) -> Double)? {didSet {setNeedsDisplay()}}
     
+    //parameters for graph
+    var scale: CGFloat = 50.0
+    var lineWidth: CGFloat = 2.0
+    var color: UIColor = UIColor.cyan
+    var colorAxes: UIColor = UIColor.brown
+    
+    //dot in center
+    var originSet: CGPoint?
+    
+    //dot in center init
+    private var origin: CGPoint
+    {
+        get {return originSet ?? CGPoint(x: self.bounds.midX, y: self.bounds.midY)}
+        set {originSet = newValue}
+    }
+    
+    private var axesDrawer = AxesDrawer()
     
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        axesDrawer.contentScaleFactor = contentScaleFactor
+        axesDrawer.color = colorAxes
+        axesDrawer.drawAxes(in: bounds, origin: origin, pointsPerUnit: scale)
     }
+    
+    
+    
  
 
 }
