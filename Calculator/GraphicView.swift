b6a8b4baf8df18bf284811c97dd2e1965f13e8bf
Zaur Giyasov <zaurusnnov@icloud.com>
2017-10-13 05:37:55 +0300

    [Assignment 3: Graphing Calculator] add view and controller

---
+++ Calculator/GraphicView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphicView.swift
+//  Calculator
+//
+//  Created by ZaurNNov on 13.10.2017.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+import UIKit
+
+class GraphicView: UIView {
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+ 
+
+}
